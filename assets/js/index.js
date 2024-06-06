f803df61a4452ac6637bdc5c3261e290dacfd168
leoyasu <leandroyasutake@gmail.com>
2024-06-06 18:57:20 -0300

    add scroll up btn

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,3 +35,21 @@ const swiper = new Swiper(".swiper", {
     disableOnInteraction: true,
   },
 });
+
+
+const backToTopButton = document.getElementById("back-to-top");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  });
+
+  backToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
